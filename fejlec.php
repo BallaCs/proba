e98a916a968acdb20f972e27c6f9c03ff9320628
Balla Csanád <balla.csanad@prioris.hu>
2019-03-24 14:53:56 +0100

    form, date picker

--- fejlec.php
+++ fejlec.php
@@ -8,6 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Document</title>
 </head>
--- fooldal-form.php
+++ fooldal-form.php
@@ -6,25 +6,53 @@
         <div class="form-group">
           <input type="text" class="form-control" id="cim" placeholder="Cím">
         </div>
-        <div class="form-row">
-          <div class="col">
+        <div class="form-row align-items-center">
+          <div class="col-6">
             <div class="custom-file">
                 <input type="file" accept=".jpg,.jpeg,.png" class="custom-file-input" id="file">
                 <label class="custom-file-label" for="file" data-browse="Tallózás...">Válassz képet</label>
             </div>
           </div>
-          <div class="col">
+          <div class="col-5">
             <select class="form-control" id="album">
               <option value="" disabled selected>Melyik albumba kerüljön?</option>
               <option>1</option>
               <option>2</option>
             </select>
           </div>
+          <div class="col-1">
+            <div class="checkbox">
+              <label style="margin-bottom:0;"><input type="checkbox"> Vers</label>
+            </div>
+          </div>
         </div>
         <div class="form-group">
           <textarea class="form-control" id="exampleFormControlTextarea1" rows="15" placeholder="Vers/Szöveg"></textarea>
         </div>
-      <button type="submit" class="btn btn-primary">Közzétesz</button>
+        <div class="bootstrap-iso">
+            <div class="form-group ">
+              <div class="input-group">
+                <input class="form-control" id="date" name="date" placeholder="MM/DD/YYYY" type="text"/>
+              </div>
+            </div>  
+        </div>'
+        ?>      
+
+        <script>
+          $(document).ready(function(){
+            var date_input=$('input[name="date"]'); //our date input has the name "date"
+            var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+            date_input.datepicker({
+              format: 'mm/dd/yyyy',
+              container: container,
+              todayHighlight: true,
+              autoclose: true,
+            })
+          })
+        </script>
+      <?php
+      echo
+      '<button type="submit" class="btn btn-primary">Közzétesz</button>
       </form>
     </div>';
   }
