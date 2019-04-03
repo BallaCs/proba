e71c792c3e5185ff3e97a9487b00bee73448bc63
Balla Csanád <balla.csanad@prioris.hu>
2019-04-03 18:12:04 +0200

    link-preview kezdetek

--- hegyongy.sql
+++ hegyongy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2019. Már 29. 15:49
+-- Létrehozás ideje: 2019. Ápr 03. 18:10
 -- Kiszolgáló verziója: 10.1.37-MariaDB
 -- PHP verzió: 7.3.1
 
@@ -42,7 +42,6 @@ INSERT INTO `album` (`Album_ID`, `albumNev`) VALUES
 (2, 'munka'),
 (3, 'Csani'),
 (4, 'Csenge'),
-(5, 'Geri'),
 (6, 'Zsolti'),
 (7, 'Apa'),
 (8, 'Csani2'),
@@ -75,13 +74,25 @@ INSERT INTO `kep` (`Kep_ID`, `utvonal`, `Album_ID`) VALUES
 (8, 'assets/kepek/5c9e01a5d62362.66615814.jpg', NULL),
 (9, 'assets/kepek/5c9e065bb77c07.01866992.jpg', NULL),
 (10, 'assets/kepek/5c9e06ad0c8290.43983966.jpg', NULL),
-(11, '', NULL),
-(12, '', NULL),
-(13, '', NULL),
 (14, 'assets/kepek/5c9e1ee1e702c3.84600899.jpg', 0),
 (15, 'assets/kepek/5c9e1f30295250.89479299.jpg', 3),
 (16, 'assets/kepek/5c9e1f72d59af7.51661776.jpg', 9),
-(17, 'assets/kepek/5c9e25b5448199.56348097.jpg', 3);
+(17, 'assets/kepek/5c9e25b5448199.56348097.jpg', 3),
+(18, 'assets/kepek/5c9f79267afed0.89628317.jpg', 3),
+(20, 'assets/kepek/5c9fbca686ef60.45743524.jpg', 3),
+(21, 'assets/kepek/5ca12e9306f503.70549799.jpg', 8),
+(22, 'assets/kepek/5ca12f6dd5c158.10761246.jpg', 1),
+(24, 'assets/kepek/5ca12f6debf182.87669063.jpg', 1),
+(25, 'assets/kepek/5ca12f6e0807e9.25155969.jpg', 1),
+(26, 'assets/kepek/5ca12f6e12b320.57029262.jpg', 1),
+(27, 'assets/kepek/5ca12f6e205b45.69828692.jpg', 1),
+(28, 'assets/kepek/5ca12f6e2b2a06.58089412.jpg', 1),
+(29, 'assets/kepek/5ca12f6e3e1544.53656773.jpg', 1),
+(37, 'assets/kepek/5ca3ad43b68127.94580529.jpg', 9),
+(38, 'assets/kepek/5ca3add11da3e4.35807409.jpg', 8),
+(39, 'assets/kepek/5ca3b0bde3cff8.09905651.jpg', 3),
+(40, 'assets/kepek/5ca3b5f0e18317.97496754.jpg', 3),
+(41, 'assets/kepek/5ca3b862b35da2.27487590.png', 6);
 
 -- --------------------------------------------------------
 
@@ -95,6 +106,7 @@ CREATE TABLE `post` (
   `szoveg` text COLLATE utf8_hungarian_ci,
   `Kep_ID` int(6) DEFAULT NULL,
   `vers` tinyint(1) DEFAULT NULL,
+  `video` varchar(20) COLLATE utf8_hungarian_ci DEFAULT NULL,
   `datum` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
@@ -102,19 +114,14 @@ CREATE TABLE `post` (
 -- A tábla adatainak kiíratása `post`
 --
 
-INSERT INTO `post` (`Post_ID`, `cim`, `szoveg`, `Kep_ID`, `vers`, `datum`) VALUES
-(1, 'cim', 'In sed nulla nulla. Vivamus sit amet tellus ut metus vulputate lacinia nec at purus. Sed eget purus urna. Nulla facilisi. Suspendisse eu pharetra tortor, eget ultrices eros. Phasellus eu elit sit amet est eleifend hendrerit ac vel nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis efficitur augue. Nulla vitae tellus risus. Morbi et cursus nunc, molestie aliquet leo.', 9, 1, '2019-03-29'),
-(2, 'cim2', 'Donec vulputate dolor vel tortor vehicula posuere. Phasellus hendrerit fermentum felis, aliquam tincidunt justo venenatis et. Vivamus nec semper justo. Quisque mattis imperdiet nibh, et efficitur enim bibendum at. Aenean vel lorem eu elit ullamcorper scelerisque. Curabitur quis blandit metus. Maecenas vitae libero neque. Maecenas ut leo et tortor cursus ornare id at nibh. Aliquam elit eros, egestas eget nibh vitae, dapibus auctor justo.', 10, 0, '2019-03-22'),
-(3, '', 'Morbi pharetra ipsum quis eros porta, vel viverra augue vehicula. Maecenas fringilla nibh quis eros viverra ultrices. Aenean euismod venenatis pretium. Aliquam blandit efficitur metus, eget sagittis velit laoreet sollicitudin. Proin dapibus purus id odio consectetur, et sodales metus volutpat. Pellentesque non ante molestie, suscipit lorem vitae, viverra orci. Vestibulum sem erat, convallis sit amet feugiat eu, volutpat at diam. Aenean nec leo vel est molestie convallis eu sed lorem. Pellentesque fringilla nunc nec augue eleifend, vel ultricies felis fermentum. Fusce rhoncus, justo vitae scelerisque ullamcorper, sem sem pharetra elit, id vulputate tellus velit vel ante. Integer tincidunt, magna non faucibus fermentum, lectus justo vulputate justo, vitae porta sapien metus non magna. Maecenas sapien metus, tempor nec erat ac, convallis facilisis velit.', 11, 0, '2019-03-29'),
-(4, '', 'Morbi pharetra ipsum quis eros porta, vel viverra augue vehicula. Maecenas fringilla nibh quis eros viverra ultrices. Aenean euismod venenatis pretium. Aliquam blandit efficitur metus, eget sagittis velit laoreet sollicitudin. Proin dapibus purus id odio consectetur, et sodales metus volutpat. Pellentesque non ante molestie, suscipit lorem vitae, viverra orci. Vestibulum sem erat, convallis sit amet feugiat eu, volutpat at diam. Aenean nec leo vel est molestie convallis eu sed lorem. Pellentesque fringilla nunc nec augue eleifend, vel ultricies felis fermentum. Fusce rhoncus, justo vitae scelerisque ullamcorper, sem sem pharetra elit, id vulputate tellus velit vel ante. Integer tincidunt, magna non faucibus fermentum, lectus justo vulputate justo, vitae porta sapien metus non magna. Maecenas sapien metus, tempor nec erat ac, convallis facilisis velit.', 12, 0, '2019-03-29'),
-(5, '', 'Morbi pharetra ipsum quis eros porta, vel viverra augue vehicula. Maecenas fringilla nibh quis eros viverra ultrices. Aenean euismod venenatis pretium. Aliquam blandit efficitur metus, eget sagittis velit laoreet sollicitudin. Proin dapibus purus id odio consectetur, et sodales metus volutpat. Pellentesque non ante molestie, suscipit lorem vitae, viverra orci. Vestibulum sem erat, convallis sit amet feugiat eu, volutpat at diam. Aenean nec leo vel est molestie convallis eu sed lorem. Pellentesque fringilla nunc nec augue eleifend, vel ultricies felis fermentum. Fusce rhoncus, justo vitae scelerisque ullamcorper, sem sem pharetra elit, id vulputate tellus velit vel ante. Integer tincidunt, magna non faucibus fermentum, lectus justo vulputate justo, vitae porta sapien metus non magna. Maecenas sapien metus, tempor nec erat ac, convallis facilisis velit.', 13, 1, '2019-03-29'),
-(6, '', 'Morbi pharetra ipsum quis eros porta, vel viverra augue vehicula. Maecenas fringilla nibh quis eros viverra ultrices. Aenean euismod venenatis pretium. Aliquam blandit efficitur metus, eget sagittis velit laoreet sollicitudin. Proin dapibus purus id odio consectetur, et sodales metus volutpat. Pellentesque non ante molestie, suscipit lorem vitae, viverra orci. Vestibulum sem erat, convallis sit amet feugiat eu, volutpat at diam. Aenean nec leo vel est molestie convallis eu sed lorem. Pellentesque fringilla nunc nec augue eleifend, vel ultricies felis fermentum. Fusce rhoncus, justo vitae scelerisque ullamcorper, sem sem pharetra elit, id vulputate tellus velit vel ante. Integer tincidunt, magna non faucibus fermentum, lectus justo vulputate justo, vitae porta sapien metus non magna. Maecenas sapien metus, tempor nec erat ac, convallis facilisis velit.', 0, 0, '2019-03-29'),
-(7, 'cim3', '', 0, 0, '2019-03-29'),
-(8, '', '', 14, 1, '2019-03-29'),
-(9, '', '', 15, 0, '2019-03-29'),
-(10, '', '', 16, 0, '2019-03-29'),
-(11, 'Vers', 'Roses are grey.\r\nViolets are grey.\r\nI am a dog.', 17, 1, '2019-03-29'),
-(12, 'sfdds', '', 0, 0, '2019-03-27');
+INSERT INTO `post` (`Post_ID`, `cim`, `szoveg`, `Kep_ID`, `vers`, `video`, `datum`) VALUES
+(3, 'CsanÃ¡d', 'Morbi pharetra ipsum quis eros porta, vel viverra augue vehicula. Maecenas fringilla nibh quis eros viverra ultrices. Aenean euismod venenatis pretium. Aliquam blandit efficitur metus, eget sagittis velit laoreet sollicitudin. Proin dapibus purus id odio consectetur, et sodales metus volutpat. Pellentesque non ante molestie, suscipit lorem vitae, viverra orci. Vestibulum sem erat, convallis sit amet feugiat eu, volutpat at diam. Aenean nec leo vel est molestie convallis eu sed lorem. Pellentesque fringilla nunc nec augue eleifend, vel ultricies felis fermentum. Fusce rhoncus, justo vitae scelerisque ullamcorper, sem sem pharetra elit, id vulputate tellus velit vel ante. Integer tincidunt, magna non faucibus fermentum, lectus justo vulputate justo, vitae porta sapien metus non magna. Maecenas sapien metus, tempor nec erat ac, convallis facilisis velit.', 37, 1, 'Ye1ir8MQS4w', '2019-03-30'),
+(5, 'CsanÃ¡d', 'Morbi pharetra ipsum quis eros porta, vel viverra augue vehicula. Maecenas fringilla nibh quis eros viverra ultrices. Aenean euismod venenatis pretium. Aliquam blandit efficitur metus, eget sagittis velit laoreet sollicitudin. Proin dapibus purus id odio consectetur, et sodales metus volutpat. Pellentesque non ante molestie, suscipit lorem vitae, viverra orci. Vestibulum sem erat, convallis sit amet feugiat eu, volutpat at diam. Aenean nec leo vel est molestie convallis eu sed lorem. Pellentesque fringilla nunc nec augue eleifend, vel ultricies felis fermentum. Fusce rhoncus, justo vitae scelerisque ullamcorper, sem sem pharetra elit, id vulputate tellus velit vel ante. Integer tincidunt, magna non faucibus fermentum, lectus justo vulputate justo, vitae porta sapien metus non magna. Maecenas sapien metus, tempor nec erat ac, convallis facilisis velit.', 37, 0, '', '2019-03-30'),
+(10, 'AkÃ¡rmi', 'Vestibulum sem erat, convallis sit amet feugiat eu, volutpat at diam. Aenean nec leo vel est molestie convallis eu sed lorem. Pellentesque fringilla nunc nec augue eleifend, vel ultricies felis fermentum. Fusce rhoncus, justo vitae scelerisque ullamcorper, sem sem pharetra elit, id vulputate tellus velit vel ante. Integer tincidunt, magna non faucibus fermentum, lectus justo vulputate justo, vitae porta sapien metus non magna. Maecenas sapien metus, tempor nec erat ac, convallis facilisis velit.', 41, 0, '', '2019-03-31'),
+(13, 'Valami', 'Morbi pharetra ipsum quis eros porta, vel viverra augue vehicula. Maecenas fringilla nibh quis eros viverra ultrices. Aenean euismod venenatis pretium. Aliquam blandit efficitur metus, eget sagittis velit laoreet sollicitudin. Proin dapibus purus id odio consectetur, et sodales metus volutpat. Pellentesque non ante molestie, suscipit lorem vitae, viverra orci. Vestibulum sem erat, convallis sit amet feugiat eu, volutpat at diam. Aenean nec leo vel est molestie convallis eu sed lorem. Pellentesque fringilla nunc nec augue eleifend, vel ultricies felis fermentum. Fusce rhoncus, justo vitae scelerisque ullamcorper, sem sem pharetra elit, id vulputate tellus velit vel ante. Integer tincidunt, magna non faucibus fermentum, lectus justo vulputate justo, vitae porta sapien metus non magna. Maecenas sapien metus, tempor nec erat ac, convallis facilisis velit.', 0, 1, '', '2019-03-21'),
+(14, 'CsanÃ¡d', 'Morbi pharetra ipsum quis eros porta, vel viverra augue vehicula. Maecenas fringilla nibh quis eros viverra ultrices. Aenean euismod venenatis pretium. Aliquam blandit efficitur metus, eget sagittis velit laoreet sollicitudin. Proin dapibus purus id odio consectetur, et sodales metus volutpat. Pellentesque non ante molestie, suscipit lorem vitae, viverra orci. Vestibulum sem erat, convallis sit amet feugiat eu, volutpat at diam. Aenean nec leo vel est molestie convallis eu sed lorem. Pellentesque fringilla nunc nec augue eleifend, vel ultricies felis fermentum. Fusce rhoncus, justo vitae scelerisque ullamcorper, sem sem pharetra elit, id vulputate tellus velit vel ante. Integer tincidunt, magna non faucibus fermentum, lectus justo vulputate justo, vitae porta sapien metus non magna. Maecenas sapien metus, tempor nec erat ac, convallis facilisis velit.', 0, 1, '', '2019-04-01'),
+(15, 'CÃ­m', 'Morbi pharetra ipsum quis eros porta, vel viverra augue vehicula. Maecenas fringilla nibh quis eros viverra ultrices. Aenean euismod venenatis pretium. Aliquam blandit efficitur metus, eget sagittis velit laoreet sollicitudin. ', 40, 0, '', '2019-04-02'),
+(17, 'CsanÃ¡d4', 'Morbi pharetra ipsum quis eros porta, vel viverra augue vehicula. Maecenas fringilla nibh quis eros viverra ultrices. Aenean euismod venenatis pretium. Aliquam blandit efficitur metus, eget sagittis velit laoreet sollicitudin. Proin dapibus purus id odio consectetur, et sodales metus volutpat. Pellentesque non ante molestie, suscipit lorem vitae, viverra orci. Vestibulum sem erat, convallis sit amet feugiat eu, volutpat at diam. Aenean nec leo vel est molestie convallis eu sed lorem. Pellentesque fringilla nunc nec augue eleifend, vel ultricies felis fermentum. Fusce rhoncus, justo vitae scelerisque ullamcorper, sem sem pharetra elit, id vulputate tellus velit vel ante. Integer tincidunt, magna non faucibus fermentum, lectus justo vulputate justo, vitae porta sapien metus non magna. Maecenas sapien metus, tempor nec erat ac, convallis facilisis velit.', 0, 1, '', '2019-04-02');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -152,13 +159,13 @@ ALTER TABLE `album`
 -- AUTO_INCREMENT a táblához `kep`
 --
 ALTER TABLE `kep`
-  MODIFY `Kep_ID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `Kep_ID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT a táblához `post`
 --
 ALTER TABLE `post`
-  MODIFY `Post_ID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `Post_ID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
